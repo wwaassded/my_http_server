74a2451867db39da88e4c3451d3b504d85906a8d
wwaassded <wwssd507@gmail.com>
2024-02-14 21:05:36 +0800

    2024-2-14

--- src/log/log.cc
+++ src/log/log.cc
@@ -6,42 +6,29 @@
 #include <unistd.h>
 #include <cassert>
 #include <chrono>
+#include <thread>
 
 namespace what::Log {
+
+static const int MAX_BUFFER_QUEUE_SIZE = 8;
+static const int VERBOSITY_NAME_LEN = 9;
 
 auto Log::GetInstance() -> Log * {
   static Log log = Log();
   return &log;
 }
 
-void Log::Init(const char *file_name, Verbosity max_verbosity, int flush_ms) {
+// TODO
+Log::Log() : is_aync(false), block_queue(nullptr) {}
+Log::~Log() {
+  delete[] start_time;
+  if (file != nullptr) fclose(file);
+}
+
+void Log::Init(const char *file_name, Verbosity max_verbosity, int max_queue_size) {
   max_verbosity_into_file = max_verbosity;
-  log_flush_internal_ms = flush_ms;
-  is_aync == log_flush_internal_ms != 0;
-  memset(dir_name, '\0', DIR_NAME_LEN);
-  memset(this->file_name, '\0', FILE_NAME_LEN);
-  if (file_name != nullptr) {  //*用户提供了日志文件的路径
-    const char *last_slash_position = strrchr(file_name, '/');
-    if (last_slash_position != nullptr) {
-      snprintf(dir_name,
-               DIR_NAME_LEN < (last_slash_position - file_name) ? DIR_NAME_LEN : (last_slash_position - file_name),
-               "%s", file_name);
-      snprintf(this->file_name, FILE_NAME_LEN, "%s", last_slash_position + 1);
-      char file_path[DIR_NAME_LEN + FILE_NAME_LEN + 1];
-      file_path[DIR_NAME_LEN + FILE_NAME_LEN] = '\0';
-      snprintf(file_path, sizeof(file_path), "%s/%s", dir_name, this->file_name);
-      if (!create_log_dir(file_path)) {
-        exit(EXIT_FAILURE);
-      }
-    } else {
-      snprintf(this->file_name, FILE_NAME_LEN, "%s", file_name);
-    }
-    file = fopen(file_name, "a");
-    if (file == nullptr) {
-      LOG(ERROR, "failed to open %s", file_name);
-      exit(EXIT_FAILURE);
-    }
-  }
+  block_queue = std::make_unique<Block_queue<std::string>>(max_queue_size);
+  is_aync = (max_queue_size != 0);
   time_since_epoch =
       std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
           .count();
@@ -49,6 +36,81 @@ void Log::Init(const char *file_name, Verbosity max_verbosity, int flush_ms) {
   auto tm = localtime(&current_time);
   start_time = malloc(sizeof(struct tm));
   memcpy(start_time, tm, sizeof(struct tm));
+  memset(dir_name, '\0', DIR_NAME_LEN);
+  memset(this->file_name, '\0', FILE_NAME_LEN);
+  if (file_name != nullptr) {  //*用户提供了日志文件的路径
+    const char *last_slash_position = strrchr(file_name, '/');
+    char file_path[DIR_NAME_LEN + FILE_NAME_LEN + 1];
+    file_path[DIR_NAME_LEN + FILE_NAME_LEN] = '\0';
+    auto time_cal = reinterpret_cast<struct tm *>(start_time);
+
+    if (last_slash_position != nullptr) {
+      snprintf(dir_name,
+               DIR_NAME_LEN < (last_slash_position - file_name) ? DIR_NAME_LEN : (last_slash_position - file_name),
+               "%s", file_name);
+      snprintf(this->file_name, FILE_NAME_LEN, "%s", last_slash_position + 1);
+      snprintf(file_path, sizeof(file_path), "%s/%s_%04d-%02d-%02d", dir_name, this->file_name,
+               time_cal->tm_year + 1900, time_cal->tm_mon + 1, time_cal->tm_mday);
+      if (!create_log_dir(file_path)) {
+        exit(EXIT_FAILURE);
+      }
+    } else {
+      snprintf(file_path, sizeof(file_path), "%s/%s_%04d-%02d-%02d", dir_name, this->file_name,
+               time_cal->tm_year + 1900, time_cal->tm_mon + 1, time_cal->tm_mday);
+    }
+    file = fopen(file_path, "a");
+    if (file == nullptr) {
+      LOG(ERROR, "failed to open %s", file_name);
+      exit(EXIT_FAILURE);
+    }
+  }
+  //* 异步日志 flush线程
+  if (this->is_aync) {
+    std::thread(&Log::flush_thread_function, this).detach();
+  }
+}
+
+void Log::Log_everywhere(Verbosity verbosity, const char *file_path, unsigned int line, const char *format, ...) {
+  // INFO:    2024-
+  // WARNING:
+  char level[VERBOSITY_NAME_LEN];
+  level[VERBOSITY_NAME_LEN - 1] = '\0';
+  switch (verbosity) {
+    case Verbosity::Verbosity_LOG: {
+      snprintf(level, VERBOSITY_NAME_LEN, "LOG:");
+      break;
+    }
+    case Verbosity::Verbosity_INFO: {
+      snprintf(level, VERBOSITY_NAME_LEN, "INFO:");
+      break;
+    }
+    case Verbosity::Verbosity_WARNING: {
+      snprintf(level, VERBOSITY_NAME_LEN, "WARNING:");
+      break;
+    }
+    case Verbosity::Verbosity_ERROR: {
+      snprintf(level, VERBOSITY_NAME_LEN, "ERROR:");
+      break;
+    }
+    case Verbosity::Verbosity_FATAL: {
+      snprintf(level, VERBOSITY_NAME_LEN, "FATAL:");
+      break;
+    }
+    default: {
+      LOG(ERROR, "unknown verbosity name!");
+    }
+  }
+  auto time_seed = time(NULL);
+  struct tm curr_tm;
+  localtime_r(&time_seed, &curr_tm);
+  //我们希望对日志进行 按照天数进行分类
+  if (curr_tm.tm_mday != reinterpret_cast<struct tm *>(start_time)->tm_mday) {
+    memcpy(start_time, &curr_tm, sizeof(struct tm));
+    assert(file != nullptr);
+    flush();
+    fclose(file);
+    //重新使用新的文件
+  }
 }
 
 auto Log::create_log_dir(char *tmp_file) -> bool {
@@ -64,6 +126,21 @@ auto Log::create_log_dir(char *tmp_file) -> bool {
     *p = '/';
   }
   return true;
+}
+
+void Log::flush_thread_function() {
+  if (!is_aync) return;
+  std::string str;
+  for (;;) {  // TODO 需要一种退出的方式
+    block_queue->Pop(str);
+    fputs(str.c_str(), file);
+  }
+}
+
+void Log::flush() {
+  locker.lock();
+  fflush(file);
+  locker.unlock();
 }
 
 }  // namespace what::Log
--- src/log/log.hpp
+++ src/log/log.hpp
@@ -3,6 +3,8 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <memory>
+#include "thread_safe_queue.hpp"
 
 namespace what::Log {
 
@@ -18,7 +20,8 @@ class Log {
  public:
   static auto GetInstance() -> Log *;
 
-  void Init(const char *file_name, Verbosity max_verbosity, int flush_ms);
+  //! 我们应该尽量保证 file_name not null, 成员函数内部可能会有某些assert要求 FILE* not null
+  void Init(const char *file_name, Verbosity max_verbosity, int max_queue_size);
 
  private:
   const static int DIR_NAME_LEN = 128;
@@ -30,17 +33,23 @@ class Log {
   char dir_name[DIR_NAME_LEN];
   char file_name[FILE_NAME_LEN];
 
+  void flush_thread_function();
+
   Verbosity max_verbosity_into_file;  //写入到文件中的最大 verbosity
 
   FILE *file{nullptr};  // 日志文件对应的文件指针
   bool is_aync{false};
-  int log_flush_internal_ms{0};
+  std::mutex locker;
   __int64_t time_since_epoch;
   void *start_time;  //! malloc
+
+  std::unique_ptr<Block_queue<std::string>> block_queue;
 
   void Log_everywhere(Verbosity, const char *file_path, unsigned int line, const char *format, ...);
 
   auto create_log_dir(char *dir) -> bool;
+
+  void flush();
 };
 
 #define VLOG(verbosity, ...) what::Log::Log::GetInstance()->Log_everywhere(verbosity, __VA_ARGS__);
